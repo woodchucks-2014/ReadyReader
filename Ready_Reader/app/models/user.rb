8950d541bd7baa622942a49bf5b9aa26b6a4aff7
& <pair+benbrostoff.greg99799@devbootcamp.com>
2014-07-24 14:54:27 -0400

    add models (and delete hammer.js gem)

---
+++ Ready_Reader/app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  has_many :books, :comments
+
+  validates_presence_of :name, :email
+  validates_uniqueness_of :email
+  validates :email, format: {with: /[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+\.[a-zA-Z]{2,}/}
+
+end
