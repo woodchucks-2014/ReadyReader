5ac2824f2581fefada8b692b5a60ae5435e7cf71
& <pair+benbrostoff.bsusensjackson@devbootcamp.com>
2014-07-24 16:37:23 -0400

    Added routes for books and comments

--- Ready_Reader/spec/features/books.rb
+++ Ready_Reader/spec/features/books.rb
@@ -3,15 +3,16 @@ require 'rails_helper'
 feature 'ability to read a sentence from a book' do
 
   before(:each) do
-    let(:book) { Book.create(name: "test", content: "test")}
+    let(:book) { FactoryGirl.create :book}
     visit books_path(1)
   end
+
   scenario 'user sees the title of the book' do
     expect(page).to have_content(book.title)
   end
 
   scenario 'user sees one sentence from the book' do
-    expect(page).to have_content(book.sentences.sample)
+    expect(page).to have_content(book.sentences[0])
   end
 
 end
