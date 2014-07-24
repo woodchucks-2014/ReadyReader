946b5632141359492ce84f9cb2a793265f1d3f9e
& <pair+benbrostoff.bsusensjackson@devbootcamp.com>
2014-07-24 16:59:02 -0400

    Pass sentence from book test

--- Ready_Reader/spec/features/books.rb
+++ Ready_Reader/spec/features/books.rb
@@ -1,15 +1,19 @@
 require 'rails_helper'
 
   feature 'ability to read a sentence from a book' do
-  let!(:book) { Book.create(title: "test", content: "test")}
+    let!(:book) { Book.create(title: "test", content: "test")}
 
-  scenario 'user sees the title of the book' do
-    visit book_path(book.id)
-    expect(page).to have_content(book.title)
-  end
+    before :each do
+      visit book_path(book.id)
+    end
 
-  scenario 'user sees one sentence from the book' do
-    expect(page).to have_content(book.sentences[0])
-  end
+    scenario 'user sees the title of the book' do
+      expect(page).to have_content(book.title)
+    end
+
+    scenario 'user sees one sentence from the book' do
+      visit book_path(book.id)
+      expect(page).to have_content(book.sentences[0])
+    end
 
 end
