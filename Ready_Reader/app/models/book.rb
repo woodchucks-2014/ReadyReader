77eff163a13ade6311c829955f28598bfa46c711
& <pair+benbrostoff.greg99799@devbootcamp.com>
2014-07-24 15:01:32 -0400

    finish user validations and add seed file

--- Ready_Reader/app/models/book.rb
+++ Ready_Reader/app/models/book.rb
@@ -1,4 +1,11 @@
 class Book < ActiveRecord::Base
   belongs_to :user
   has_many :comments
+  before_save :sentence_parse
+
+  def sentence_parse
+    tokenizer = TactfulTokenizer::Model.new
+    self.sentences = tokenizer.tokenize_text(self.content)
+  end
+
 end
