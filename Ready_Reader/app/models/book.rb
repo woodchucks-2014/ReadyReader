3dd8f8b79ebbee1d068ffb9e1c3790799842a875
Greg <knudsegi@hotmail.com>
2014-07-25 19:15:10 -0400

    can parse long sentences in half in sandbox - WIP on doing in app

--- Ready_Reader/app/models/book.rb
+++ Ready_Reader/app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
   before_save :sentence_parse
-  #before_save :long_parse(150)
+  before_save :long_parse
 
 
   def sentence_parse #detrimental to perf. time
@@ -10,16 +10,19 @@ class Book < ActiveRecord::Base
     self.sentences = tokenizer.tokenize_text(self.content)
   end
 
-  # def long_parse(max)
-  #   self.sentences.each_with_index do |sentence, index|
-  #     if sentence.length > max
-  #       word_array = sentence.split(" ") #array of words
-  #       word_array = word_array.each_slice(70) #array of arrays
-  #       =
-
-
-  #     end
-  # end
+  def long_parse
+    first_half = []
+    second_half = []
+    self.sentences.each do |sentence|
+      if sentence.length > 15
+        word_array = sentence.split(" ") #array of words
+        first_half << word_array[0..(word_array.length/2)]
+        second_half << word_array[(word_array.length/2 + 1)..-1]
+      end
+    first_half.flatten.join(' ')
+    second_half.flatten.join(' ')
+    end
+  end
 
   def pages
     self.sentences.size
