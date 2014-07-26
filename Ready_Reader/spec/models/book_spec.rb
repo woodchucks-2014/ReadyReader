80a31ca319c0461be0b6e314dc9150bb5d08568c
Ben Brostoff <ben.brostoff@gmail.com>
2014-07-26 00:24:28 -0400

    All tests pass excepting long_parse

--- Ready_Reader/spec/models/book_spec.rb
+++ Ready_Reader/spec/models/book_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Book, :type => :model do
   end
 
   context "#long parse"
-      let(:book_2) {Book.create(title: "Test", sentences: "If you're trying to use features that don't seem to be in the latest released gem, but are mentioned in this README, then you probably need to specify the master branch if you want to use them.")}
+      let(:book_2) {Book.create(title: "Test", content: "If you're trying to use features that don't seem to be in the latest released gem, but are mentioned in this README, then you probably need to specify the master branch if you want to use them.")}
 
     it "takes a sentence that is over 150 characters long and breaks it in half" do
       book_2.long_parse
