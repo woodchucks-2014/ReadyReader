d44c125a46ae1e749b2768b46ed79c83413dcc37
Ben Brostoff <ben.brostoff@gmail.com>
2014-07-26 20:59:41 -0400

    Debugged errors in comment ability; modals on comments no longer disappear after first comment

--- Ready_Reader/app/assets/javascripts/comments.js
+++ Ready_Reader/app/assets/javascripts/comments.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
 
   $('#add_comment').on("click", function(e) {
+    $('#form-fun').show();
     e.preventDefault();
     console.log("FUCK YOU HAMMER");
+    console.log($('.current_sentence').text());
+
     $('#commentModal').modal();
   })
 
@@ -11,14 +14,15 @@ $(document).ready(function(){
     var ajaxRequest = $.ajax({
       url : '/comment_on_book',
       data: {commentary: $('#comment_commentary').val(), commented_on: $('.current_sentence').text()},
-
       method: 'POST'
     });
 
     ajaxRequest.success(function(response){
       console.log("yolo");
-      $('#form_fun').remove();
-      $('#new_comment').show();
+      $('#commentModal').modal('hide');
+      $('body').removeClass('modal-open');
+      $('.modal-backdrop').remove();
+      // $('#new_comment').show();
 
     });
 
