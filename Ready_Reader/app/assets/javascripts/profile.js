0b015182ed5fea5027f08ba2f65837aa9fe1c3d9
Ben Brostoff & Greg <pair+benbrostoff.greg99799@devbootcamp.com>
2014-07-28 01:53:53 -0400

    Profile page now displays progress for all books

--- Ready_Reader/app/assets/javascripts/profile.js
+++ Ready_Reader/app/assets/javascripts/profile.js
@@ -1,8 +1,17 @@
-  $(function() {
-    for (i =0; i < +$('.book_count_user').text(); i++) {
-      console.log(i);
-      $( ".progress_bar_index" ).progressbar({
-        value: +$('#bar' + i).text()
+var barProfileProgress = function(progress, i){
+    $(function() {
+      $( ".progress_bar_index" + i ).progressbar({
+        value: progress
       });
-    };
+    });
+};
+
+$(document).ready(function() {
+   for (i =0; i < +$('.book_count_user').text(); i++) {
+      progress = +$('#bar' + i).text()
+      console.log(i);
+      a = i
+      //console.log(progress)
+      barProfileProgress(progress, i);
+    }
   });
