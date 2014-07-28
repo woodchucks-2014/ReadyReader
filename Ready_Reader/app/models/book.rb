3cc9b35b520d134bda53874053d8f0a4792d8416
Ben Brostoff <ben.brostoff@gmail.com>
2014-07-28 17:49:04 -0400

    User profile page issues resolved

--- Ready_Reader/app/models/book.rb
+++ Ready_Reader/app/models/book.rb
@@ -1,12 +1,9 @@
 class Book < ActiveRecord::Base
+
   belongs_to :user
   has_many :comments, dependent: :destroy
 
   has_many :user_books
   has_many :users, through: :user_books
-
-  # def sentence_parse #detrimental to perf. time
-  #   self.sentences = tokenizer.tokenize_text(self.content) #initialize TT via initializer
-  # end
 
 end
