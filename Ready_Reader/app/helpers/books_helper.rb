b45cf92114211c420949d7fcdc0f1e4c67cf41cf
Greg <knudsegi@hotmai.com>
2014-07-27 20:19:42 -0400

    Added title parsing helper and method in user_books to prevent overshooting array end

--- Ready_Reader/app/helpers/books_helper.rb
+++ Ready_Reader/app/helpers/books_helper.rb
@@ -4,4 +4,12 @@ module BooksHelper
     Book.find(session[:book]).title if session[:book]
   end
 
+  def title_parse(title)
+    if title.length < 12
+      return title
+    else
+      return title[0..12] + "..."
+    end
+  end
+
 end
