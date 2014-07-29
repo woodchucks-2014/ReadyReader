dbf4ba661ceb0d224f43483daf0b469e0bf241a6
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2014-07-29 12:28:00 -0400

    Sentences now show up - took out sentence objects completely and working on book MVC

--- Ready_Reader/app/assets/javascripts/books.js
+++ Ready_Reader/app/assets/javascripts/books.js
@@ -1,270 +1,291 @@
-// Books
-var Book = function(current, sentence){
-  this.current = current;
-  this.end = +$('.pages').text();
-  this.sentence = sentence;
-}
+// // Books
+// var Book = function(current, sentence, current){ // BOOK MODEL
+//   this.current = current;
+//   this.end = +$('.pages').text();
+//   //this.sentence = sentence;
+//   this.index = current;
+// }
 
-Book.prototype.checkForEnd = function(){
-  if (this.current === this.end) {
-   $('.error_reading').text("<- End")
-  } else {$('.error_reading').text("")};
-    $('.current_sentence').text(this.sentence.currentSentence(this.current));
-}
+// Book.prototype.checkForEnd = function(){ // BOOK CONTROLLER
+//   if (this.current === this.end) {
+//    $('.error_reading').text("<- End")
+//   } else {$('.error_reading').text("")};
+//     $('.current_sentence').text(this.sentence.currentSentence(this.current));
+// }
 
-Book.prototype.checkForBeginning = function(){
-  if (this.current === 0) {
-    $('.error_reading').text("Begin ->");
-  } else {$('.error_reading').text("")}
-  $('.current_sentence').text(this.sentence.currentSentence(this.current));
-}
+// Book.prototype.checkForBeginning = function(){ // BOOK CONTROLLER
+//   if (this.current === 0) {
+//     $('.error_reading').text("Begin ->");
+//   } else {$('.error_reading').text("")}
+//   $('.current_sentence').text(this.sentence.currentSentence(this.current));
+// }
 
-// Sentences
-var Sentence = function(book, current){
-   this.book = book;
-   this.pages = +$('.pages').text();
-   this.index = current;
-}
-//Hide all noncurrent sentence - elements on the DOM
-Sentence.prototype.hideNoncurrent = function(){
-    $('non_current_sentence').hide();
-}
-//Increment Function
-Sentence.prototype.increment = function() {
-   this.index += 1
-   this.book.current = this.index;
-   if (this.index >= this.pages) { // should never be outside range
-    this.index = this.pages;
-    this.book.current = this.pages;
-   }
-}
-//Decriment function.
-Sentence.prototype.decrement = function() {
-  this.index -= 1
-  this.book.current = this.index;
-  if (this.index < 0 && this.index != this.book.end) {
-    this.index = 0;
-    this.book.current = 0;
-  }
-}
-
-Sentence.prototype.currentSentence = function(index) {
-  return $('.sentence' + index).text();
-}
-
-Sentence.prototype.barProgress = function(current, end){
-   $(function() {
-    $( "#progressbar" ).progressbar({
-      value: (current / end) * 100,
-    });
-  });
-}
-
-Sentence.prototype.currentSentence = function(index) {
-  return $('.sentence' + index).text();
-}
-
-// Slider
-var sliderProgress = function(current, pages){
-
-  $("#slider_bar").slider({
-    range: "min",
-    value: current,
-    min: 0,
-    max: pages,
-    step: 1,
-    slide: function( event, ui ) {
-            $( "#amount" ).val( ui.value );
-            $('.text_progress').text("Sentence " + ui.value + " of " + pages );
-            $('.percentage').text(parseInt((ui.value / pages) * 100) + '%'  )
-        },
-        create: function(event, ui) {
-            var v= $(this).slider('value');
-            $('.text_progress').text("Sentence " + v + " of " + book.end );
-            $('.percentage').text(parseInt((v / book.end) * 100) + '%'  )
-      }
-  });
-}
-
-// localStorage
-var getBookId = function () {return +$('.book_number').text(); }
-
-// var localStorageInit = function(){
-//   var book_id = +$('.book_number').text();
-//   var user_name = $('.user_name').text();
-//   var userObject = {userName: user_name, bookId: book_id, currentSentence: 0};
-
-//   if (localStorage[user_name + book_id] === undefined) {
-//      localStorage.setItem(user_name + book_id, JSON.stringify(userObject));
+// // Sentences
+// var Sentence = function(book, current){ // SENTENCE MODEL
+//    this.book = book;
+//    this.pages = +$('.pages').text(); // this is repetive, as book has same attribute
+//    this.index = current;
+// }
+// //Hide all noncurrent sentence - elements on the DOM
+// Sentence.prototype.hideNoncurrent = function(){ // SENTENCE VIEW
+//     $('non_current_sentence').hide();
+// }
+// //Increment Function
+// Sentence.prototype.increment = function() { // SENTENCE MODEL
+//    this.index += 1
+//    this.book.current = this.index;
+//    if (this.index >= this.pages) { // should never be outside range
+//     this.index = this.pages;
+//     this.book.current = this.pages;
+//    }
+// }
+// //Decriment function.
+// Sentence.prototype.decrement = function() { // SENTENCE MODEL
+//   this.index -= 1
+//   this.book.current = this.index; // SOME DEPENDENCY HERE, DISCUSS
+//   if (this.index < 0 && this.index != this.book.end) {
+//     this.index = 0;
+//     this.book.current = 0; // SEE EARLEIR COMMENT
 //   }
+// }
+
+// Sentence.prototype.currentSentence = function(index) { // SENTENCE VIEW
+//   return $('.sentence' + index).text();
+// }
+
+// Sentence.prototype.barProgress = function(current, end){ // SENTENCE VIEW
+//    $(function() {
+//     $( "#progressbar" ).progressbar({
+//       value: (current / end) * 100,
+//     });
+//   });
+// }
+
+// // Slider
+// var sliderProgress = function(current, pages){ // MODULE? DISCUSS
+
+//   $("#slider_bar").slider({
+//     range: "min",
+//     value: current,
+//     min: 0,
+//     max: pages,
+//     step: 1,
+//     slide: function( event, ui ) {
+//             $( "#amount" ).val( ui.value );
+//             $('.text_progress').text("Sentence " + ui.value + " of " + pages );
+//             $('.percentage').text(parseInt((ui.value / pages) * 100) + '%'  )
+//         },
+//         create: function(event, ui) {
+//             var v= $(this).slider('value');
+//             $('.text_progress').text("Sentence " + v + " of " + book.end );
+//             $('.percentage').text(parseInt((v / book.end) * 100) + '%'  )
+//       }
+//   });
+// }
+
+// // localStorage
+// var getBookId = function () {return +$('.book_number').text(); } // BOOK VIEW
+
+// // var localStorageInit = function(){
+// //   var book_id = +$('.book_number').text();
+// //   var user_name = $('.user_name').text();
+// //   var userObject = {userName: user_name, bookId: book_id, currentSentence: 0};
+
+// //   if (localStorage[user_name + book_id] === undefined) {
+// //      localStorage.setItem(user_name + book_id, JSON.stringify(userObject));
+// //   }
+// // };
+
+// // var localStorageUpdate = function(book){
+// //   var book_id = +$('.book_number').text();
+// //   var user_name = $('.user_name').text();
+// //   var userObject = {userName: user_name, bookId: book_id, currentSentence: book.current};
+// //   localStorage.setItem(user_name + book_id, JSON.stringify(userObject));
+// // }
+
+// // PageTurn module
+// var PageTurn = {  // THIS MAKES SENSE TO ME AND I AM OKAY WITH KEEPING
+
+//   left: function(sentence, book) {
+//       sentence.increment();
+//       book.checkForEnd();
+
+//       sentence.barProgress(book.current, book.end);
+//       sliderProgress(book.current, book.end);
+
+//       Refresh.hideShow;
+//       Refresh.progress(book)
+//       localStorageUpdate(book);
+//   },
+
+//   right: function(sentence, book) {
+//       sentence.decrement();
+//       book.checkForBeginning();
+//       sentence.barProgress(book.current, book.end);
+//       sliderProgress(book.current, book.end);
+
+//       Refresh.hideShow();
+//       Refresh.progress(book);
+//       localStorageUpdate(book);
+//   }
+// }
+
+// // Refresh module
+// var Refresh = { // DO WE NEED HIDE SHOW TO REFRESH CSS?
+//   hideShow: function(){
+//     $('.slider_bar').hide();
+//     $('.slider_bar').show();
+//   },
+
+//   progress: function(book){
+//     $('.percentage').text(parseInt((book.current / book.end) * 100) + '%'  )
+//     $('.text_progress').text("Sentence " + book.current + " of " + book.end )
+//   }
+// }
+
+// // class refreshes
+// // var initializeBook = function() {
+// //   $('.non_current_sentence').hide();
+// //   $('.sentence_wrapper').hide();
+// //   $('.sentence_wrapper').show();
+// //   $('.ui-slider-handle').hide();
+// //   $('.ui-slider-handle').show();
+// // }
+
+// // AJAX callback setup
+// var get_cp = function(argument) {
+//   console.log("EXECUTION");
+//   var keyLook = $('.user_name').text() + +$('.book_number').text()
+//   console.log(keyLook);
+//     return $.ajax({
+//     url : '/check_point',
+//     method : 'POST',
+//     data : { object: JSON.parse(localStorage[keyLook]) },
+//     success : function(response){
+//     //setTimeout(function(){get_cp();}, 400);
+//     }
+//   });
 // };
 
-// var localStorageUpdate = function(book){
-//   var book_id = +$('.book_number').text();
-//   var user_name = $('.user_name').text();
-//   var userObject = {userName: user_name, bookId: book_id, currentSentence: book.current};
-//   localStorage.setItem(user_name + book_id, JSON.stringify(userObject));
+// // function ajaxInterval(request){
+// //   timeOutId = setTimeout(request, 1000);
+// // }
+// var Book = function(start, end) {
+//   this.start = 0;
+//   this.end = end;
+//   this.sentences = [];
+
+// };
+
+// Book.prototype.currentPosition = function() {
+
+
 // }
 
-// PageTurn module
-var PageTurn = {
 
-  left: function(sentence, book) {
-      sentence.increment();
-      book.checkForEnd();
+// var BookView = function() {
+//   var book = new Book();
+// };
 
-      sentence.barProgress(book.current, book.end);
-      sliderProgress(book.current, book.end);
-
-      Refresh.hideShow;
-      Refresh.progress(book)
-      localStorageUpdate(book);
-  },
-
-  right: function(sentence, book) {
-      sentence.decrement();
-      book.checkForBeginning();
-      sentence.barProgress(book.current, book.end);
-      sliderProgress(book.current, book.end);
-
-      Refresh.hideShow();
-      Refresh.progress(book);
-      localStorageUpdate(book);
-  }
-}
-
-// Refresh module
-var Refresh = {
-  hideShow: function(){
-    $('.slider_bar').hide();
-    $('.slider_bar').show();
-  },
-
-  progress: function(book){
-    $('.percentage').text(parseInt((book.current / book.end) * 100) + '%'  )
-    $('.text_progress').text("Sentence " + book.current + " of " + book.end )
-  }
-}
-
-// class refreshes
-var initializeBook = function() {
-  $('.non_current_sentence').hide();
-  $('.sentence_wrapper').hide();
-  $('.sentence_wrapper').show();
-  $('.ui-slider-handle').hide();
-  $('.ui-slider-handle').show();
-}
-
-// AJAX callback setup
-var get_cp = function(argument) {
-  console.log("EXECUTION");
-  var keyLook = $('.user_name').text() + +$('.book_number').text()
-  console.log(keyLook);
-    return $.ajax({
-    url : '/check_point',
-    method : 'POST',
-    data : { object: JSON.parse(localStorage[keyLook]) },
-    success : function(response){
-    //setTimeout(function(){get_cp();}, 400);
-    }
-  });
-};
-
-// function ajaxInterval(request){
-//   timeOutId = setTimeout(request, 1000);
+// BookView.prototype.showCurrentSentence = function() {
+//   $('.non_current_sentence').hide();
+//   $('.current_sentence').text();
 // }
 
-var BookView = function() {
-  var book = new Book();
-};
+// BookView.prototype.bookId = function() {
+//   return +$('.book_number').text();
+// }
 
-BookView.prototype.showCurrentSentence = function() {
-  $('.non_current_sentence').hide();
-  $('.current_sentence').text();
-}
+// BookView.prototype.userName = function() {
+//   return $('.user_name').text();
+// }
 
-BookView.prototype.bookId = function() {
-  return +$('.book_number').text();
-}
+// BookView.prototype.initialize = function() {
+//   $('.non_current_sentence').hide();
+//   $('.sentence_wrapper').hide();
+//   $('.sentence_wrapper').show();
+//   $('.ui-slider-handle').hide();
+//   $('.ui-slider-handle').show();
+// }
 
-BookView.prototype.userName = function() {
-  return $('.user_name').text();
-}
+// var lS = function (){
+//   this.initialize = function (bookview) {
+//     var book_id = bookview.bookId;
+//     var user_name = bookview.userName;
+//     var userObject = {userName: user_name, bookId: book_id, currentSentence: 0};
+//     localStorage.setItem(user_name + book_id, JSON.stringify(userObject));
+//     if (localStorage[user_name + book_id] === undefined) {
+//      localStorage.setItem(user_name + book_id, JSON.stringify(userObject));
+//     }
+//   },
 
-var lS = function (){
-  this.initialize = function (bookview) {
-    var book_id = bookview.bookId;
-    var user_name = bookview.userName;
-    var userObject = {userName: user_name, bookId: book_id, currentSentence: 0};
-    localStorage.setItem(user_name + book_id, JSON.stringify(userObject));
-    if (localStorage[user_name + book_id] === undefined) {
-     localStorage.setItem(user_name + book_id, JSON.stringify(userObject));
-    }
-  },
+//   this.update = function(book, bookview) {
+//     // var book_id = bookview.bookId;
+//     // var user_name = bookview.userName;
+//     // var userObject = {userName: user_name, bookId: book_id, currentSentence: book.current};
+//     // localStorage.setItem(user_name + book_id, JSON.stringify(userObject));
 
-  this.update = function(book, bookview) {
-    var book_id = bookview.bookId;
-    var user_name = bookview.userName;
-    var userObject = {userName: user_name, bookId: book_id, book.current};
-    localStorage.setItem(user_name + book_id, JSON.stringify(userObject));
-
-  }
-}
+//   }
+// }
 
 
 $(document).ready(function() {
-  bookView = new BookView();
-  lS = new lS();
-  lS.initialize(bookView);
-  console.log(localStorage);
+  bookController = new BookController();
+  bookController.initialize();
+  // bookView = new BookView();
+  // bookView.initialize();
+  // lS = new lS();
+  // lS.initialize(bookView);
+  // console.log(localStorage);
+  // bookView.showCurrentSentence();
 
   // setTimeout = (get_cp, 400);
   // localStorageInit();
-  initializeBook();
+
   //ajaxInterval(get_cp());
 
-  var positionUpdate = function(){
-    return get_cp();
-  }
+ //  var positionUpdate = function(){
+ //    return get_cp();
+ //  }
 
- positionUpdate().done(function(result){ //may need slight tweaks.
-    book = new Book(result.farthest_point, new Sentence());
-    sentence = new Sentence(book, book.current);
-    console.log(book);
-    sliderProgress(book.current, book.end);
-    sentence.barProgress(book.current, book.end);
+ // positionUpdate().done(function(result){ //may need slight tweaks.
+ //    book = new Book(result.farthest_point, new Sentence());
+ //    sentence = new Sentence(book, book.current);
+ //    console.log(book);
+ //    sliderProgress(book.current, book.end);
+ //    sentence.barProgress(book.current, book.end);
 
-    $('.current_sentence').text(sentence.currentSentence(book.current));
-    $('.progress_bar').show();
-    Refresh.progress(book)
-    ls.update(book);
-  })
+ //    $('.current_sentence').text(sentence.currentSentence(book.current));
+ //    $('.progress_bar').show();
+ //    Refresh.progress(book)
+ //    lS.update(book);
+ //  })
 
   // fast forward and rewind
-  $('#slider_bar').mouseup(function() {
-    var newPoint = $(this).slider('value');
+  // $('#slider_bar').mouseup(function() {
+  //   var newPoint = $(this).slider('value');
 
-    $('.current_sentence').text(sentence.currentSentence(newPoint));
+  //   $('.current_sentence').text(sentence.currentSentence(newPoint));
 
-    sentence.index = newPoint;
-    book.current = newPoint;
+  //   sentence.index = newPoint;
+  //   book.current = newPoint;
 
-    console.log("SUSENS SUCKS");
-    console.log(book);
+  //   // console.log("SUSENS SUCKS");
+  //   // console.log(book);
 
-    Refresh.hideShow();
-    Refresh.progress(book);
-    localStorageUpdate(book);
-  });
+  //   Refresh.hideShow();
+  //   Refresh.progress(book);
+  //   localStorageUpdate(book);
+  // });
 
   // Please note this is temporary until we can include Hammer.js
-   $(".right").on("click", function() {
-      PageTurn.right(sentence, book);
-    });
+   // $(".right").on("click", function() {
+   //    PageTurn.right(sentence, book);
+   //  });
 
-    $(".left").on("click", function() {
-      PageTurn.left(sentence, book);
-    });
+   //  $(".left").on("click", function() {
+   //    PageTurn.left(sentence, book);
+   //  });
   // end of temporary code
 
   // Once asset pipeline issues are resolved, here's the code:
@@ -279,13 +300,13 @@ $(document).ready(function() {
   //   get_cp();
   // });
 
-  function swipeleftHandler(){
-    PageTurn.left(sentence, book);
-  }
+//   function swipeleftHandler(){
+//     PageTurn.left(sentence, book);
+//   }
 
-  function swiperightHandler() {
-    PageTurn.right(sentence, book);
-  }
+//   function swiperightHandler() {
+//     PageTurn.right(sentence, book);
+//   }
 });
 
 
--- Ready_Reader/app/assets/javascripts/mvc_js.js
+++ Ready_Reader/app/assets/javascripts/mvc_js.js
@@ -1,37 +1,96 @@
-// var Sentence = function() {}
+// BOOK MODEL
 
+var Book = function(pages) {
+  this.start = 0;
+  this.current = 0; //look into better (smarter) implementation for current.
+  this.end = pages; // hidden in DOM, reliant on view
+};
 
+Book.prototype.checkForEnd = function() {
+  if (this.current >= this.end) {
+    this.current = this.end
+  }
+}
 
-// var Book = function(start, end) {
-//   this.start = 0;
-//   this.end = end;
-//   this.sentences = sentences; //maybe -
-// };
+Book.prototype.checkForBeginning = function() {
+  if (this.current <= 0){
+    this.current = 0;
+  }
+}
 
+Book.prototype.turnPageLeft = function() {
+  this.current += 1
+}
 
-// Book.prototype.checkForEnd = function() {
+Book.prototype.turnPageRight = function() {
+  this.current -= 1
+}
 
-// }
+// BOOK CONTROLLER
 
-// Book.prototype.checkForBeginning = function() {
+var BookController = function() {
+  var bookview = new BookView();
+  var book = new Book(bookview.getPages()); // book.current will update on AJAX request
 
-// }
+  var turnPageRight = function() {
+    book.turnPageRight();
+    book.checkForEnd();
 
-// Book.prototype.tunPageLeft = function() {
+    bookview.showCurrentSentence(book.current);
 
-// }
+  }
 
-// Book.prototype.turnPageRight = function() {
+  var turnPageLeft = function() {
+    book.turnPageLeft();
+    book.checkForBeginning();
 
-// }
+    bookview.showCurrentSentence(book.current);
+  }
 
-// Book.prototype.incrementSentence = function() {
+  this.initialize = function () {
+    bookview.hideNonCurrent();
+    bookview.showCurrentSentence(book.current);
 
-// }
+    bookview.getPage().on("click", ".right", turnPageRight)
+    bookview.getPage().on("click", ".left", turnPageLeft)
+  }
+};
 
-// Book.prototype.decrimentSentence = function(){
+// BOOK VIEW
 
-// }
+var BookView = function () {}
+
+BookView.prototype.getCurrentText = function(index) {
+  return $('.sentence' + index).text();
+}
+
+BookView.prototype.showCurrentSentence = function(index) {
+  console.log("*************")
+  console.log(index)
+  text = this.getCurrentText(index);
+  console.log(text)
+  $('.current_sentence').text(text);
+}
+
+BookView.prototype.bookId = function() { // LOCAL STORAGE RELATED
+  return +$('.book_number').text();
+}
+
+BookView.prototype.reader = function() { // LOCAL STORAGE RELATED
+  return $('.user_name').text();
+}
+
+BookView.prototype.hideNonCurrent = function(){
+  $('.non_current_sentence').hide();
+}
+
+BookView.prototype.getPages = function(){
+  return +$('.pages').text();
+}
+
+BookView.prototype.getPage = function(){
+  return $(document);
+}
 
 // var lS = function (){
 //   function initialize(bookview) {
@@ -41,23 +100,3 @@
 //     localStorage.setItem(user_name + book_id, JSON.stringify(userObject));
 //   }
 // }
-
-// var BookView = function() {
-//   var book = new Book();
-// };
-
-// BookView.prototype.showCurrentSentence = function() {
-//   $('.non_current_sentence').hide();
-//   $('.current_sentence').text();
-// }
-
-// BookView.prototype.bookId = function() {
-//   return +$('.book_number').text();
-// }
-
-// BookView.prototype.userName = function() {
-//   return $('.user_name').text();
-
-// }
-
-
