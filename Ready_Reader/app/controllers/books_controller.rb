cc0d4326ed141ed2ded2085c755fe7b4dd4216ca
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2014-07-30 11:13:38 -0400

    sentence worker working, needs tweaks

--- Ready_Reader/app/controllers/books_controller.rb
+++ Ready_Reader/app/controllers/books_controller.rb
@@ -22,26 +22,30 @@ class BooksController < ApplicationController
   def upload
     @user = User.find(params[:user_id])
     uploaded_io = params[:book]
-    @filename = Rails.root.join('public', 'uploads', uploaded_io.original_filename)
-    File.open(@filename, 'wb') do |file|
+    filename = Rails.root.join('public', 'uploads', uploaded_io.original_filename)
+    File.open(filename, 'wb') do |file|
       file.write(uploaded_io.read)
     end
 
-    Resque.enqueue(UploadWorker, params)
 
 
-    # book = EPUB::Parser.parse(filename)
-    # content = ""
 
-    # book.each_page_on_spine do |page|
-    #   page = page.content_document.nokogiri
-    #   page.search('p').each{|el| el.before ' '}
-    #   content << page.text
-    # end
+    book = EPUB::Parser.parse(filename)
+    content = ""
 
-    # book = Book.create(title: params[:title], content: content)
-    # @user.books << book
+    book.each_page_on_spine do |page|
+      page = page.content_document.nokogiri
+      page.search('p').each{|el| el.before ' '}
+      content << page.text
+    end
 
+    @book = Book.new
+    @book.title = params[:title]
+    @book.content = content
+    @book.save!
+
+    @user.books << @book
+    Resque.enqueue(SentenceWorker, @book.id)
     # content_array = tokenize_special(book.content)
     # content_array.each do |sentence|
     #   Sentence.create(book_id: book.id, content: sentence)
@@ -91,7 +95,7 @@ class BooksController < ApplicationController
   private
 
   def book_params(params)
-    params.require(:book).permit(:title, :content)
+    params.require(:book).permit(:title)
   end
 
 
--- Ready_Reader/app/workers/upload_worker.rb
+++ Ready_Reader/app/workers/upload_worker.rb
@@ -1,28 +1,28 @@
-class UploadWorker
-  @queue = :upload
+# class UploadWorker
+#   @queue = :upload
 
-  def self.perform(info)
-    uploaded_io = params[:book]
-    @filename = Rails.root.join('public', 'uploads', uploaded_io.original_filename) #look into this tomorrow.
-    File.open(@filename, 'wb') do |file|
-      file.write(uploaded_io.read)
-    end
-    @user = User.find(info[:user_id])
-    book = EPUB::Parser.parse(info[:file])
-    content = ""
+#   def self.perform(info)
+#     uploaded_io = params[:book]
+#     @filename = Rails.root.join('public', 'uploads', uploaded_io.original_filename) #look into this tomorrow.
+#     File.open(@filename, 'wb') do |file|
+#       file.write(uploaded_io.read)
+#     end
+#     @user = User.find(info[:user_id])
+#     book = EPUB::Parser.parse(info[:file])
+#     content = ""
 
-    book.each_page_on_spine do |page|
-      page = page.content_document.nokogiri
-      page.search('p').each{|el| el.before ' '}
-      content << page.text
-    end
+#     book.each_page_on_spine do |page|
+#       page = page.content_document.nokogiri
+#       page.search('p').each{|el| el.before ' '}
+#       content << page.text
+#     end
 
-    @book = Book.create(title: "fun", content: content)
-    # UserBook.create(user_id: @user.id, book_id: @book.id)
-    # @user.books << @book
-    # content_array = tokenize_special(@book.content)
-    # content_array.each do |sentence|
-    #   Sentence.create(book_id: @book.id, content: sentence)
-    # end
-  end
-end
+#     @book = Book.create(title: "fun", content: content)
+#     # UserBook.create(user_id: @user.id, book_id: @book.id)
+#     # @user.books << @book
+#     # content_array = tokenize_special(@book.content)
+#     # content_array.each do |sentence|
+#     #   Sentence.create(book_id: @book.id, content: sentence)
+#     # end
+#   end
+# end
