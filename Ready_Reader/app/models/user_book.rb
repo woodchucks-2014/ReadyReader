200fd35db65ab587856db594d3b49420eafc8e22
Ben Brostoff <ben.brostoff@gmail.com>
2014-07-30 00:18:43 -0400

    Additional formatting and added percentage signs to profile page / changed some divs to spans to account for JS

--- Ready_Reader/app/models/user_book.rb
+++ Ready_Reader/app/models/user_book.rb
@@ -3,9 +3,6 @@ class UserBook < ActiveRecord::Base
   belongs_to :book
 
   def local_storage_comp(user_id, local_val)
-    p "*"
-    p "THIS IS THE FARTHEST POINT PRE UPDATE"
-    p self.farthest_point
     self.farthest_point = local_val.to_i if local_val.to_i > self.farthest_point
     save_point = self.farthest_point if user_id != 1
     save_point = local_val if user_id == 1
