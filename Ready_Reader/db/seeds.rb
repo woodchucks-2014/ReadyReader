6232471fdb2dc5236d47ce0be2f90db71bb20b4c
Greg <knudsegi@hotmail.com>
2014-07-30 15:36:06 -0400

    Added date to datetime parser (was previously only returning dates in certain conditions

--- Ready_Reader/db/seeds.rb
+++ Ready_Reader/db/seeds.rb
@@ -1,45 +1,39 @@
 include Tact_Token
 
-promo = Book.create(title: "ReadyReader", content: "Tap by tap, ReadyReader delivers your books to you sentence by sentence. Upload your favorite books and read them on your phone. On the train with no internet connection? ReadyReader lets you read individual books offline and saves your progress using persistent local storage in HTML5. Don't believe us? Try closing out of this browser now and opening back up to this page. Sign up now and get reading!", universal: true)
+promo = Book.create(title: "ReadyReader", content: "Tap by tap, ReadyReader delivers your books to you sentence by sentence. Upload your favorite books and read them on your phone. On the train with no internet connection? ReadyReader lets you read individual books offline and saves your progress using persistent local storage in HTML5. Don't believe us? Try closing out of this browser now and opening back up to this page. Sign up now and get reading!")
 
-hp = Book.create(title: "Harry Potter and the Deathly Hallows", content: "It's the final book! Harry finds three things that allow him to defeat Voldemort. Then he sees his parents in pseudo heaven... blah blah blah. Mr. Potter is not liked by Professor Snape!")
-sound_fury = Book.create(title: "The Sound and the Fury", content: "Benny has no idea what's going on. The family is fighting! The Old South is full of sound and fury. The end.")
-infinite_jest = Book.create(title: "Infinite Jest", content: "Really meaningful stuff. Here's a super long sentence that our app should parse and if it doesn't you probably know that sentence went wrong, DFW likes to write long sentences but that's fine since he at one point was considered the best writer of our time blah blah blah blah. The end")
+universal_books = [['public/uploads/orwell-animal-farm.epub', "Animal Farm"], ['public/uploads/gatsby.epub', "The Great Gatsby"]]
 
-filename = 'public/uploads/orwell-animal-farm.epub'
+universal_books.each do |filename|
+  book = EPUB::Parser.parse(filename[0])
+  content = ""
 
-p "EXECUTION"
-book = EPUB::Parser.parse(filename)
-content = ""
-p "EXECUTION"
+  book.each_page_on_spine do |page|
+    page = page.content_document.nokogiri
+    page.search('p').each{|el| el.before ' '}
+    content << page.text
+  end
 
-book.each_page_on_spine do |page|
-  page = page.content_document.nokogiri
-  page.search('p').each{|el| el.before ' '}
-  content << page.text
+  new_book = Book.create(title: filename[1], content: content, universal: true)
+
+  content_array = tokenize_special(new_book.content)
+  content_array.each do |sentence|
+    Sentence.create(book_id: new_book.id, content: sentence)
+  end
 end
 
-animal_farm = Book.create(title: "Animal Farm", content: content, universal: true)
+#promo user
+sentences = tokenize_special(promo.content)
+sentences.each { |sentence| Sentence.create(book_id: promo.id, content: sentence)}
 
-content_array = tokenize_special(animal_farm.content)
-content_array.each do |sentence|
-  Sentence.create(book_id: animal_farm.id, content: sentence)
-end
-
-
-books = [promo, hp, sound_fury, infinite_jest]
-books.each do |book|
-  sentences = tokenize_special(book.content)
-  sentences.each { |sentence| Sentence.create(book_id: book.id, content: sentence)}
-end
 
 promo_user = User.create(name: "user", email: "user@user.com", password: "test", password_confirmation: "test")
 ben = User.create(name: "Ben", email: "ben@ben.com", password: "test", password_confirmation: "test")
 greg = User.create(name: "Greg", email: "greg@greg.com", password: "test", password_confirmation: "test")
 
 promo_user.books << promo
-ben.books << [hp, infinite_jest]
-greg.books << sound_fury
+
+
 
 
 
