fd10193f128677ce09bf6487e9ca7d6805b7ecce
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2014-07-30 12:21:43 -0400

    moved tokenize back to module

--- Ready_Reader/app/workers/sentence_worker.rb
+++ Ready_Reader/app/workers/sentence_worker.rb
@@ -1,33 +1,12 @@
 class SentenceWorker
-  # include Tact_Token
+  extend Tact_Token
   @queue = :sentences
 
   def self.perform(id)
     @book = Book.find(id)
-    # binding.pry
     content_array = tokenize_special(@book.content)
     content_array.each do |sentence|
       Sentence.create(book_id: @book.id, content: sentence)
     end
-
-  end
-
-  # $tt ||= TactfulTokenizer::Model.new
-
-  def self.tokenize_special(content)
-    tt = TactfulTokenizer::Model.new
-    sentence_array = tt.tokenize_text(content)
-    sentence_array.each_with_index do |sentence, index|
-      word_array = sentence.split(" ") #array of words
-        if word_array.length > 20 #there are ~5 letters per word, we max out at 100 characters
-          divisor = word_array.length/20
-          sliced_sentence_array = word_array.each_slice((word_array.size / divisor).round).to_a
-          sliced_sentence_array.map! { |sentence| sentence.join(" ") }
-          sentence_array[index] = sliced_sentence_array
-
-        end
-      #p array
-    end
-    return sentence_array.flatten
   end
 end
--- Ready_Reader/lib/core_ext/tact_token.rb
+++ Ready_Reader/lib/core_ext/tact_token.rb
@@ -6,20 +6,12 @@ module Tact_Token
     sentence_array = $tt.tokenize_text(content)
      sentence_array.each_with_index do |sentence, index|
       word_array = sentence.split(" ") #array of words
-      p "&" * 100
-      p word_array
         if word_array.length > 20 #there are ~5 letters per word, we max out at 100 characters
           divisor = word_array.length/20
           sliced_sentence_array = word_array.each_slice((word_array.size / divisor).round).to_a
           sliced_sentence_array.map! { |sentence| sentence.join(" ") }
           sentence_array[index] = sliced_sentence_array
-          p "UF"
-          p "*" * 100
-          p sentence_array
         end
-      p "THIS IS GETTING CALLED"
-      p "*" * 100
-      #p array
       end
       return sentence_array.flatten
     end
