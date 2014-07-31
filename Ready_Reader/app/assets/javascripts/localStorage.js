7490b56c8b798e8a5065c98e3ac4593e397ad3eb
Ben Brostoff <ben.brostoff@gmail.com>
2014-07-31 18:49:48 -0400

    Small cleanups in localStorage.js

--- Ready_Reader/app/assets/javascripts/localStorage.js
+++ Ready_Reader/app/assets/javascripts/localStorage.js
@@ -8,10 +8,13 @@ LocalStorage = {
     var user_name = bookview.reader();
     var userObject = {userName: user_name, bookId: book_id, currentSentence: 0};
 
+    if (book_id === 0) {
+      var userObject = {userName: Math.random().toString(), bookId: 0, currentSentence: 0};
+      localStorage.setItem(user_name + book_id, JSON.stringify(userObject));
+    }
     if (localStorage[user_name + book_id] === undefined) {
       localStorage.setItem(user_name + book_id, JSON.stringify(userObject));
     }
-    console.log(localStorage)
   },
 
   update : function(book, bookview) {
@@ -21,6 +24,11 @@ LocalStorage = {
     var book_id = bookview.bookId();
     var user_name = bookview.reader();
     var userObject = {userName: user_name, bookId: book_id, currentSentence: book.current};
+
+    if (book_id === 0) {
+      var userObject = {userName: Math.random().toString(), bookId: 0, currentSentence: 0};
+      localStorage.setItem(user_name + book_id, JSON.stringify(userObject));
+    }
 
     localStorage.setItem(user_name + book_id, JSON.stringify(userObject));
 
