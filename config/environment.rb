425d23b905448dae7a30d75421c5e1dfbaceceb6
Ben Brostoff <ben.brostoff@gmail.com>
2014-07-30 21:24:13 -0400

    Moved config down

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
-config.gem 'redis'
 
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+config.gem 'redis'
